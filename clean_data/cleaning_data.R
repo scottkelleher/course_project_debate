2e0aae33e208254fcfd55ae22e542afcb4eded72
Janalee Thompson <janaleethompson7@gmail.com>
2016-11-09 11:59:41 -0700

    end of class update

--- clean_data/cleaning_data.R
+++ clean_data/cleaning_data.R
@@ -16,7 +16,23 @@ debate_lines <- lapply(c("CLINTON:", "TRUMP:"), getLines)
 library(tidytext)
 library(tokenizers)
 
+library(dplyr)
+text_df <- data_frame(text_c = clinton_lines[[1]])
+text_df
+
+text_debate1 %>%
+  unnest_tokens(word, text_c)
+text_debate1
+
+
 clinton_lines <- debate_lines[1]
 trump_lines <- debate_lines[2]
 
+library(dplyr)
+library(stringr)
 
+text_debate1 %>%
+  dplyr::group_by(clinton_lines) %>%
+  mutate(linenumber = row_number()) %>%
+  ungroup()
+text_debate1
