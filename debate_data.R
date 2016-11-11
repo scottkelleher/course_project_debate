af49e94d235c1210b3ae736532cc5e6f05d6b4fe
Janalee Thompson <janaleethompson7@gmail.com>
2016-11-11 12:58:30 -0700

    making trump and clinton lines

--- debate_data.R
+++ debate_data.R
@@ -3,6 +3,25 @@ library(tidyverse)
 library(stringr)
 library(tidytext)
 
+text_debate1 <- read_html("http://www.presidency.ucsb.edu/ws/index.php?pid=118971") # load the first debate page
+text_debate2 <- read_html("http://www.presidency.ucsb.edu/ws/index.php?pid=119038")  # load the second debate page
+text_debate3 <- read_html("http://www.presidency.ucsb.edu/ws/index.php?pid=119039")# load the third debate page 
+
+text_debate1 <- html_nodes(text_debate1, ".displaytext") %>% # isloate the text
+
+html_text() # get the text
+
+html_nodes(text_debate2, ".displaytext") %>% # isloate the text
+  
+html_text() # get the text
+
+html_nodes(text_debate3, ".displaytext") %>% # isloate the text
+  
+
+html_text() # get the text
+
+
+
 getLines <- function(person){
   text <- text_debate1 
   id <- unlist(stringr::str_extract_all(text, "[A-Z]+:")) # get the speaker
@@ -15,19 +34,22 @@ debate_lines <- lapply(c("CLINTON:", "TRUMP:"), getLines)
 clinton_lines <- debate_lines[1]
 trump_lines <- debate_lines[2]
 
-#trying to break whole text into individual words
 library(tidytext)
 library(tokenizers)
 
+#break into clinton lines
 library(dplyr)
-text_df <- data_frame(text_c = clinton_lines[[1]])
-text_df
+text_clinton <- data_frame(text_c = clinton_lines[[1]])
+text_clinton
 
-text_debate1 %>%
-  unnest_tokens(word, text_c)
-text_debate1
+#break into trump lines
+library(dplyr)
+text_trump <- data_frame(text_t = trump_lines[[1]])
+text_trump
 
-
+text_trump%>%
+  unnest_tokens(word, text_t)
+text_trump
 
 
 library(dplyr)
@@ -43,4 +65,6 @@ text_debate1
 
 ##Test
 
-
+library(tm)
+library(SnowballC)
+library(wordcloud)
