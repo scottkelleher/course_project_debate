2c2873655311e2b823147bfd4dd210a66a46de00
Scott Kelleher <smkellehermt@gmail.com>
2016-11-20 12:33:22 -0700

    use_this_for_shiny

--- debate_script_with_gtrendsR.R
+++ debate_script_with_gtrendsR.R
@@ -17,6 +17,8 @@ library(gtrendsR)
 ls("package:gtrendsR")
 
 library(RTextTools)
+source("functions/classify_emotion.R")
+
 
 
 
@@ -183,10 +185,8 @@ some_clinton_words <- gtrends(c("women", "undocumented", "security", "espionage"
   #data <- readLines("https://www.r-bloggers.com/wp-content/uploads/2016/01/vent.txt") # from: http://www.wvgazettemail.com/
   
   df <- data.frame(trump_lines)
-  f <- colnames(df)
-  #df <- rename(df, 
-   #            data = f)
-  #textdata <- df[df, ] 
+  colnames(df) <- c("col1")
+  textdata <- df[df$col1, ] 
   textdata = gsub("[[:punct:]]", "", textdata) 
   
   textdata = gsub("[[:punct:]]", "", textdata)
@@ -224,18 +224,25 @@ some_clinton_words <- gtrends(c("women", "undocumented", "security", "espionage"
   
   detach("package:sentiment", unload=TRUE)                
   
-  pdf(file.pdf,width=6,height=4,paper='special') 
+ # pdf(plots.pdf) 
   ggplot(sent_df, aes(x=emotion)) +
     geom_bar(aes(y=..count.., fill=emotion)) +
     scale_fill_brewer(palette="Dark2") +
     labs(x="emotion categories", y="", title = paste0("trump_words_from_debate_",i))
- dev.off()
+# dev.off()
   
   
 }
   
   
   
+only_trump_said <- filter(all_debate_words_trump,  !(all_debate_words_trump$word %in% all_debate_words_clinton$word))
+only_hilary_said <- filter(all_debate_words_clinton,  !(all_debate_words_clinton$word %in% all_debate_words_trump$word))
+  
+
+some_trump_words <- gtrends(c("deals", "catastrophically", "borders", "amendment"), geo = "US", start_date = "2016-09-01", end_date = "2016-11-15")
+plot(some_trump_words)
+
   
   
   
@@ -262,67 +269,3 @@ some_clinton_words <- gtrends(c("women", "undocumented", "security", "espionage"
   
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  classify_emotion <- function(textColumns,algorithm="bayes",prior=1.0,verbose=FALSE,...) {
-    matrix <- create_matrix(textColumns,...)
-    lexicon <- read.csv(system.file("data/emotions.csv.gz",package="sentiment"),header=FALSE)
-    
-    counts <- list(anger=length(which(lexicon[,2]=="anger")),disgust=length(which(lexicon[,2]=="disgust")),fear=length(which(lexicon[,2]=="fear")),joy=length(which(lexicon[,2]=="joy")),sadness=length(which(lexicon[,2]=="sadness")),surprise=length(which(lexicon[,2]=="surprise")),total=nrow(lexicon))
-    documents <- c()
-    
-    for (i in 1:nrow(matrix)) {
-      if (verbose) print(paste("DOCUMENT",i))
-      scores <- list(anger=0,disgust=0,fear=0,joy=0,sadness=0,surprise=0)
-      doc <- matrix[i,]
-      words <- findFreqTerms(doc,lowfreq=1)
-      
-      for (word in words) {
-        for (key in names(scores)) {
-          emotions <- lexicon[which(lexicon[,2]==key),]
-          index <- pmatch(word,emotions[,1],nomatch=0)
-          if (index > 0) {
-            entry <- emotions[index,]
-            
-            category <- as.character(entry[[2]])
-            count <- counts[[category]]
-            
-            score <- 1.0
-            if (algorithm=="bayes") score <- abs(log(score*prior/count))
-            
-            if (verbose) {
-              print(paste("WORD:",word,"CAT:",category,"SCORE:",score))
-            }
-            
-            scores[[category]] <- scores[[category]]+score
-          }
-        }
-      }
-      
-      if (algorithm=="bayes") {
-        for (key in names(scores)) {
-          count <- counts[[key]]
-          total <- counts[["total"]]
-          score <- abs(log(count/total))
-          scores[[key]] <- scores[[key]]+score
-        }
-      } else {
-        for (key in names(scores)) {
-          scores[[key]] <- scores[[key]]+0.000001
-        }
-      }
-      
-      best_fit <- names(scores)[which.max(unlist(scores))]
-      if (best_fit == "disgust" && as.numeric(unlist(scores[2]))-3.09234 < .01) best_fit <- NA
-      documents <- rbind(documents,c(scores$anger,scores$disgust,scores$fear,scores$joy,scores$sadness,scores$surprise,best_fit))
-    }
-    
-    colnames(documents) <- c("ANGER","DISGUST","FEAR","JOY","SADNESS","SURPRISE","BEST_FIT")
-    return(documents)
-  }          
