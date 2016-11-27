2896b0d50706e05a29eb2196d7af1eb4d9bde329
Janalee Thompson <janaleethompson7@gmail.com>
2016-11-27 11:57:28 -0700

    shiny

--- Shiny App/ui.R
+++ Shiny App/ui.R
@@ -3,40 +3,38 @@ library(shiny)
 library(ggplot2)
 
 ##start user interface
-shinyUI(fluidPage(
-  titlePanel("By Term"),
-  sidebarLayout(
-    sidebarPanel(
-    selectInput(" Search Term(s)", style = "text-align:center;color:#FFA319;font-size:150%"),
-    helpText("Give one or more terms that you want R to retrieve data from the Google Trends API.
-             Use comma to separate terms", style = "text-align:center"),
-    
-    textInput('terms', ''),
-      selectInput("speakerInput", "Speaker", choices = c("Hillary_Clinton","Donald_Trump"),
-                  selectInput("debateInput", "Debate", choices = c("First_Debate","Second_Debate", "Third_Debate"),
-                              textInput("termInput", 'terms', ''),
-                              mainPanel(
-                              plotOutput("terms"),
-        tags$h1(submitButton("Update!"), style = "text-align:center"),
-        helpText("To get results, click the 'Update!' button", style = "text-align:center"),
+shinyUI(fluidPage(  
+  #titlePanel("By Term"),
+  #sidebarLayout(   
+    #sidebarPanel(  
+    #selectInput(""),
+    #helpText("Give one or more terms that you want R to retrieve data from the Google Trends API.
+             #Use comma to separate terms"), 
+    ##textInput('terms', ''),
+       selectInput("Speaker", "Speaker", choices = c("Hillary_Clinton","Donald_Trump"),
+       selectInput("Debate", "Debate", choices = c("First_Debate","Second_Debate", "Third_Debate"),
+                              #textInput("termInput", 'terms', ''), 
   #####
   ##  Main Panel
-    mainPanel(    
-      h3(textOutput("Google Trends Analytics", align = "center"),
-      plotOutput("term_plot"),
-      plotOutput("emotions")
-      ##plotOutput("high_frequency_words")
-  ))
-  )
-  ))))
+    mainPanel(plotOutput("emotions") 
+    )))))
 
 
+#plotOutput("term_plot"), 
+      #plotOutput("emotions") 
+      ##plotOutput("high_frequency_words") 
+
+
+
+# mainPanel(
+#   #plotOutput("terms"),
+#   tags$h1(submitButton("Update!"),
+#           helpText("To get results, click the 'Update!' button"),
   
   #might need this as skeleton code later
 #   server <- function(input, output, session){
 # output$terms <- renderPlot({
 #   plot(rnorm(input$speakerInput))
-# })
-# }
+# 
     
  
