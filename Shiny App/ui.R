9f24b161573876f770f70f3862e65e7a7a2156d3
Janalee Thompson <janaleethompson7@gmail.com>
2016-11-29 17:46:10 -0700

    g

--- Shiny App/ui.R
+++ Shiny App/ui.R
@@ -9,31 +9,29 @@ shinyUI(fluidPage(
   titlePanel("Google Trends"),
 
 #Sidebar with options
+
   #sidebarLayout(   
     #sidebarPanel(
-      selectInput("Debate", "Debate", choices = c("First_Debate","Second_Debate", "Third_Debate"),
-      selectInput("Speaker", "Speaker", choices = c("Hillary_Clinton","Donald_Trump"))),
-    #helpText("Give one or more terms that you want R to retrieve data from the Google Trends API. Use comma to separate terms"),
-    ##textInput('terms', ''),
+      selectInput("Debate", "Debate", choices = c("First_Debate","Second_Debate", "Third_Debate")),
+      selectInput("Speaker", "Speaker", choices = c("Hillary_Clinton","Donald_Trump")),
 
-  ##  Main Panel, show the plots
-  mainPanel(
-    plotOutput("emotions")
-    #plotOutput("term_plot"))
-    )))
+textInput("text", label = h3("show me Google"), value = "Enter text..."),
+
+hr(),
+fluidRow(column(3, verbatimTextOutput("value"))),
 
 
+      
+  ##  Main Panel
+    mainPanel(plotOutput("emotions"), 
+              dataTableOutput("high_frequency_words"), 
+              plotOutput("word_plot"),
+              plotOutput("term_plot")
+             
+    ))) 
 
-# mainPanel(
-#   #plotOutput("terms"),
-#   tags$h1(submitButton("Update!"),
-#           helpText("To get results, click the 'Update!' button"),
-  
-  #might need this as skeleton code later
-#   server <- function(input, output, session){
-# output$terms <- renderPlot({
-#   plot(rnorm(input$speakerInput))
-# 
-    
+
+
+
 
  
